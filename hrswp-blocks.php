0d210eb04c0baab6c8d1bc21ceb5937615101d44
Adam Turner <act@adamturner.dev>
2021-07-12 11:35:37 -0700

    Bump WP tested-to to 5.8.0

--- hrswp-blocks.php
+++ hrswp-blocks.php
@@ -8,7 +8,7 @@
  * Plugin URI: https://github.com/washingtonstateuniversity/hrswp-plugin-blocks
  * Text Domain: hrswp-blocks
  * Requires at least: 5.7
- * Tested up to: 5.7.2
+ * Tested up to: 5.8.0
  * Requires PHP: 7.0
  *
  * @package HRSWP_Blocks
