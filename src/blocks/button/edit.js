bfb9e6fa3b5e2c3b80f062c52ba0855ecdf82b53
Adam Turner <act@adamturner.dev>
2022-01-13 08:42:28 -0800

    Remove unused parts and selected styles

--- src/blocks/button/edit.js
+++ src/blocks/button/edit.js
@@ -11,7 +11,6 @@ const { useCallback, useEffect, useState, useRef } = wp.element;
 const {
 	Button,
 	ButtonGroup,
-	Notice,
 	PanelBody,
 	TextControl,
 	ToolbarButton,
@@ -69,113 +68,6 @@ function WidthPanel( { selectedWidth, setAttributes } ) {
 		</PanelBody>
 	);
 }
-
-// function URLPicker( {
-// 	isSelected,
-// 	url,
-// 	setAttributes,
-// 	opensInNewTab,
-// 	onToggleOpenInNewTab,
-// 	anchorRef,
-// } ) {
-// 	const [ isURLPickerOpen, setIsURLPickerOpen ] = useState( false );
-// 	const urlIsSet = !! url;
-// 	const urlIsSetandSelected = urlIsSet && isSelected;
-// 	const openLinkControl = () => {
-// 		setIsURLPickerOpen( true );
-// 		return false; // prevents default behaviour for event
-// 	};
-// 	const unlinkButton = () => {
-// 		setAttributes( {
-// 			url: undefined,
-// 			linkTarget: undefined,
-// 			rel: undefined,
-// 		} );
-// 		setIsURLPickerOpen( false );
-// 	};
-// 	const msg = __(
-// 		'Avoid opening links in new windows/tabs. It can be disorienting and presents accessibility issues (see WCAG 2.0 G200).'
-// 	);
-// 	const linkControl = ( isURLPickerOpen || urlIsSetandSelected ) && (
-// 		<Popover
-// 			position="bottom center"
-// 			onClose={ () => setIsURLPickerOpen( false ) }
-// 			anchorRef={ anchorRef?.current }
-// 		>
-// 			<__experimentalLinkControl
-// 				className="wp-block-navigation-link__inline-link-input"
-// 				value={ { url, opensInNewTab } }
-// 				onChange={ ( {
-// 					url: newURL = '',
-// 					opensInNewTab: newOpensInNewTab,
-// 				} ) => {
-// 					setAttributes( { url: newURL } );
-
-// 					if ( opensInNewTab !== newOpensInNewTab ) {
-// 						onToggleOpenInNewTab( newOpensInNewTab );
-// 					}
-// 				} }
-// 			/>
-// 			{ opensInNewTab && (
-// 				<div className="block-editor-new-tab-checker">
-// 					<Notice
-// 						spokenMessage={ msg }
-// 						status="warning"
-// 						isDismissible={ false }
-// 					>
-// 						<>
-// 							{ msg }
-// 							<ExternalLink
-// 								href={
-// 									'https://hrscms.hrs.wsu.edu/accessibility-guide/link-text/#opening-links-in-browsers-new-tabs'
-// 								}
-// 							>
-// 								{ 'Refer to the HRS Style Guide' }
-// 							</ExternalLink>
-// 						</>
-// 					</Notice>
-// 				</div>
-// 			) }
-// 		</Popover>
-// 	);
-// 	return (
-// 		<>
-// 			<BlockControls>
-// 				<ToolbarGroup>
-// 					{ ! urlIsSet && (
-// 						<ToolbarButton
-// 							name="link"
-// 							icon={ link }
-// 							title={ __( 'Link' ) }
-// 							shortcut={ displayShortcut.primary( 'k' ) }
-// 							onClick={ openLinkControl }
-// 						/>
-// 					) }
-// 					{ urlIsSetandSelected && (
-// 						<ToolbarButton
-// 							name="link"
-// 							icon={ linkOff }
-// 							title={ __( 'Unlink' ) }
-// 							shortcut={ displayShortcut.primaryShift( 'k' ) }
-// 							onClick={ unlinkButton }
-// 							isActive={ true }
-// 						/>
-// 					) }
-// 				</ToolbarGroup>
-// 			</BlockControls>
-// 			{ isSelected && (
-// 				<KeyboardShortcuts
-// 					bindGlobal
-// 					shortcuts={ {
-// 						[ rawShortcut.primary( 'k' ) ]: openLinkControl,
-// 						[ rawShortcut.primaryShift( 'k' ) ]: unlinkButton,
-// 					} }
-// 				/>
-// 			) }
-// 			{ linkControl }
-// 		</>
-// 	);
-// }
 
 function ButtonEdit( props ) {
 	const {
@@ -238,9 +130,6 @@ function ButtonEdit( props ) {
 	const ref = useRef();
 	const richTextRef = useRef();
 	const blockProps = useBlockProps( { ref, onKeyDown } );
-	// 	const msg = __(
-	// 		'Avoid opening links in new windows/tabs. It can be disorienting and presents accessibility issues (see WCAG 2.0 G200).'
-	// 	);
 
 	const [ isEditingURL, setIsEditingURL ] = useState( false );
 	const isURLSet = !! url;
@@ -311,6 +200,15 @@ function ButtonEdit( props ) {
 				{ ! isURLSet && (
 					<ToolbarButton
 						name="link"
+						icon={ link }
+						title={ __( 'Link' ) }
+						shortcut={ displayShortcut.primary( 'k' ) }
+						onClick={ startEditing }
+					/>
+				) }
+				{ isURLSet && (
+					<ToolbarButton
+						name="link"
 						icon={ linkOff }
 						title={ __( 'Unlink' ) }
 						shortcut={ displayShortcut.primaryShift( 'k' ) }
@@ -348,26 +246,6 @@ function ButtonEdit( props ) {
 						} }
 						forceIsEditingLink={ isEditingURL }
 					/>
-					{/* { opensInNewTab && (
-						<div className="block-editor-new-tab-checker">
-							<Notice
-								spokenMessage={ msg }
-								status="warning"
-								isDismissible={ false }
-							>
-								<>
-									{ msg }
-									<ExternalLink
-										href={
-											'https://hrscms.hrs.wsu.edu/accessibility-guide/link-text/#opening-links-in-browsers-new-tabs'
-										}
-									>
-										{ 'Refer to the HRS Style Guide' }
-									</ExternalLink>
-								</>
-							</Notice>
-						</div>
-					) } */}
 				</Popover>
 			) }
 
