9ad30ad5edceda768231aed2df74a86d1501eccb
Adam Turner <act@adamturner.dev>
2022-01-21 10:24:21 -0800

    Restore class-based layout styles for the buttons block

--- src/blocks/buttons/save.js
+++ src/blocks/buttons/save.js
@@ -1,9 +1,27 @@
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
  * WordPress dependencies
  */
 const { useBlockProps, useInnerBlocksProps } = wp.blockEditor;
 
-export default function save() {
-	const innerBlocksProps = useInnerBlocksProps.save( useBlockProps.save() );
+export default function save( props ) {
+	const {
+		attributes: { layout = {} },
+		className,
+	} = props;
+	const { orientation, justifyContent } = layout;
+
+	const wrapperClasses = classnames( className, {
+		[ `is-content-justification-${ justifyContent }` ]: justifyContent,
+		[ `is-vertical` ]: orientation,
+	} );
+
+	const blockProps = useBlockProps.save( { className: wrapperClasses } );
+	const innerBlocksProps = useInnerBlocksProps.save( blockProps );
+
 	return <div { ...innerBlocksProps } />;
 }
