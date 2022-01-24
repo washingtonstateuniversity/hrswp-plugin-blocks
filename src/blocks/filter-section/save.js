e8860a237ca4572918fd878a10817338665b3478
Adam Turner <act@adamturner.dev>
2022-01-24 10:02:22 -0800

    Fix #83 update `useInnerBlockProps` for WP 5.9

--- src/blocks/filter-section/save.js
+++ src/blocks/filter-section/save.js
@@ -1,14 +1,11 @@
 /**
  * WordPress dependencies
  */
-const { InnerBlocks, useBlockProps } = wp.blockEditor;
+const { useInnerBlocksProps, useBlockProps } = wp.blockEditor;
 
 export default function save() {
-	return (
-		<>
-			<div { ...useBlockProps.save() }>
-				<InnerBlocks.Content />
-			</div>
-		</>
-	);
+	const blockProps = useBlockProps.save();
+	const innerBlocksProps = useInnerBlocksProps.save( blockProps );
+
+	return <div { ...innerBlocksProps } />;
 }
