faff8a2d06107ccac8f3522a8db525b06c3ebfa0
Adam Turner <act@adamturner.dev>
2022-01-25 11:54:02 -0800

    Bump WP required version to 5.9

--- hrswp-blocks.php
+++ hrswp-blocks.php
@@ -8,7 +8,7 @@
  * Plugin URI: https://github.com/washingtonstateuniversity/hrswp-plugin-blocks
  * Update URI: https://api.github.com/repos/washingtonstateuniversity/hrswp-plugin-blocks/releases/latest
  * Text Domain: hrswp-blocks
- * Requires at least: 5.7
+ * Requires at least: 5.9
  * Tested up to: 5.8.2
  * Requires PHP: 7.0
  *
