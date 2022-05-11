efa54aa30df0778641cf6c252378dbed83b69fad
Adam Turner <act@adamturner.dev>
2022-05-11 10:05:07 -0700

    Update callout block for WP Block API v2

--- src/blocks/callout/index.js
+++ src/blocks/callout/index.js
@@ -1,7 +1,7 @@
 /**
  * WordPress dependencies
  */
-const { __, _x } = wp.i18n;
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
@@ -11,17 +11,12 @@ import metadata from './block.json';
 import save from './save';
 import icon from './icon';
 
-const { name, category, supports } = metadata;
+const { name } = metadata;
 
-export { name };
+export { metadata, name };
 
 export const settings = {
-	title: __( 'Callout' ),
 	icon,
-	category,
-	description: __( 'Display content in a callout module.' ),
-	keywords: [ __( 'callout' ), __( 'message' ) ],
-	supports,
 	example: {
 		innerBlocks: [
 			{
@@ -29,37 +24,25 @@ export const settings = {
 				attributes: {
 					fontSize: 'large',
 					/* translators: example text. */
-					content: __( 'Riverside Wren (Cantorchilus semibadius)' ),
+					content: __('Riverside Wren (Cantorchilus semibadius)'),
 				},
 			},
 			{
 				name: 'core/paragraph',
 				attributes: {
 					/* translators: example text. */
-					content: __(
-						'The wren<br>Earns his living<br>Noiselessly.'
-					),
+					content: __('The wren<br>Earns his living<br>Noiselessly.'),
 				},
 			},
 			{
 				name: 'core/paragraph',
 				attributes: {
 					/* translators: example text. */
-					content: __( '— Kobayashi Issa (一茶)' ),
+					content: __('— Kobayashi Issa (一茶)'),
 				},
 			},
 		],
 	},
-	styles: [
-		{
-			name: 'default',
-			label: _x( 'Default', 'block style' ),
-			isDefault: true,
-		},
-		{ name: 'positive', label: _x( 'Positive', 'block style' ) },
-		{ name: 'caution', label: _x( 'Caution', 'block style' ) },
-		{ name: 'warning', label: _x( 'Warning', 'block style' ) },
-	],
 	edit,
 	save,
 };
