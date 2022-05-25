ca319adec5ade39a86c1959b1eec38cefb161a81
Adam Turner <act@adamturner.dev>
2022-05-25 11:29:00 -0700

    Fix broken heading drowdown

--- src/blocks/accordions/heading-level-dropdown.js
+++ src/blocks/accordions/heading-level-dropdown.js
@@ -1,32 +1,30 @@
 /**
  * WordPress dependencies
  */
-const { Dropdown, Toolbar, ToolbarButton, ToolbarGroup } = wp.components;
-const { __, sprintf } = wp.i18n;
-const { DOWN } = wp.keycodes;
+import { ToolbarDropdownMenu } from '@wordpress/components';
+import { __, sprintf } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
 import HeadingLevelIcon from './heading-level-icon';
 
-const HEADING_LEVELS = [ 1, 2, 3, 4, 5, 6 ];
+const HEADING_LEVELS = [ 2, 3, 4, 5, 6 ];
 
 const POPOVER_PROPS = {
 	className: 'block-library-heading-level-dropdown',
-	isAlternate: true,
 };
 
-/** @typedef {WPComponent} WPComponent */
+/** @typedef {import('@wordpress/element').WPComponent} WPComponent */
 
 /**
  * HeadingLevelDropdown props.
  *
  * @typedef WPHeadingLevelDropdownProps
  *
- * @property {number}   selectedLevel The chosen heading level.
- * @property {Function} onChange      Callback to run when
- *                                    toolbar value is changed.
+ * @property {number}                 selectedLevel The chosen heading level.
+ * @property {(newValue:number)=>any} onChange      Callback to run when
+ *                                                  toolbar value is changed.
  */
 
 /**
@@ -38,59 +36,33 @@ const POPOVER_PROPS = {
  */
 export default function HeadingLevelDropdown( { selectedLevel, onChange } ) {
 	return (
-		<Dropdown
+		<ToolbarDropdownMenu
 			popoverProps={ POPOVER_PROPS }
-			renderToggle={ ( { onToggle, isOpen } ) => {
-				const openOnArrowDown = ( event ) => {
-					if ( ! isOpen && event.keyCode === DOWN ) {
-						event.preventDefault();
-						event.stopPropagation();
-						onToggle();
-					}
-				};
+			icon={ <HeadingLevelIcon level={ selectedLevel } /> }
+			label={ __( 'Change heading level' ) }
+			controls={ HEADING_LEVELS.map( ( targetLevel ) => {
+				{
+					const isActive = targetLevel === selectedLevel;
 
-				return (
-					<ToolbarButton
-						aria-expanded={ isOpen }
-						aria-haspopup="true"
-						icon={ <HeadingLevelIcon level={ selectedLevel } /> }
-						label={ __( 'Change heading level' ) }
-						onClick={ onToggle }
-						onKeyDown={ openOnArrowDown }
-						showTooltip
-					/>
-				);
-			} }
-			renderContent={ () => (
-				<Toolbar
-					className="block-library-heading-level-toolbar"
-					label={ __( 'Change heading level' ) }
-				>
-					<ToolbarGroup
-						isCollapsed={ false }
-						controls={ HEADING_LEVELS.map( ( targetLevel ) => {
-							const isActive = targetLevel === selectedLevel;
-							return {
-								icon: (
-									<HeadingLevelIcon
-										level={ targetLevel }
-										isPressed={ isActive }
-									/>
-								),
-								title: sprintf(
-									// translators: %s: heading level e.g: "1", "2", "3"
-									__( 'Heading %d' ),
-									targetLevel
-								),
-								isActive,
-								onClick() {
-									onChange( targetLevel );
-								},
-							};
-						} ) }
-					/>
-				</Toolbar>
-			) }
+					return {
+						icon: (
+							<HeadingLevelIcon
+								level={ targetLevel }
+								isPressed={ isActive }
+							/>
+						),
+						label: sprintf(
+							// translators: %s: heading level e.g: "1", "2", "3"
+							__( 'Heading %d' ),
+							targetLevel
+						),
+						isActive,
+						onClick() {
+							onChange( targetLevel );
+						},
+					};
+				}
+			} ) }
 		/>
 	);
 }
