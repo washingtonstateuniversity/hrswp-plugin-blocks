d8b03ccb171803a08a4cd714a7efee284f8497cb
Adam Turner <act@adamturner.dev>
2022-10-18 10:00:54 -0700

    Add todo item

--- src/blocks/salary-data/index.js
+++ src/blocks/salary-data/index.js
@@ -9,6 +9,8 @@ const { name } = metadata;
 
 export { metadata, name };
 
+/* @todo Try reworking the salary data and job class blocks on the WP query block to get around the data leak issue */
+
 export const settings = {
 	icon,
 	example: {},
