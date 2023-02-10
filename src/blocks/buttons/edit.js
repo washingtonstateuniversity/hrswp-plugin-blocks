b8cfea6bbb92f918ff6e770b61a8e67a9e9e96f8
Adam Turner <act@adamturner.dev>
2023-02-10 10:30:25 -0800

    Add contentPosition attribute to buttons block

--- src/blocks/buttons/edit.js
+++ src/blocks/buttons/edit.js
@@ -7,11 +7,14 @@ import classnames from 'classnames';
  * WordPress dependencies
  */
 import {
+	BlockControls,
 	useBlockProps,
 	useInnerBlocksProps,
+	__experimentalBlockAlignmentMatrixControl as BlockAlignmentMatrixControl,
 	store as blockEditorStore,
 } from '@wordpress/block-editor';
 import { useSelect } from '@wordpress/data';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
@@ -20,17 +23,34 @@ import { name as buttonBlockName } from '../button';
 
 const ALLOWED_BLOCKS = [ buttonBlockName ];
 
+const POSITION_CLASSNAMES = {
+	'top left': 'is-position-top-left',
+	'top center': 'is-position-top-center',
+	'top right': 'is-position-top-right',
+	'center left': 'is-position-center-left',
+	'center center': 'is-position-center-center',
+	'center right': 'is-position-center-right',
+	'bottom left': 'is-position-bottom-left',
+	'bottom center': 'is-position-bottom-center',
+	'bottom right': 'is-position-bottom-right',
+};
+
 function ButtonsEdit( props ) {
 	const {
-		attributes: { layout = {} },
+		attributes: { layout = {}, contentPosition },
+		setAttributes,
 		className,
 	} = props;
 	const { orientation, justifyContent } = layout;
 
-	const wrapperClasses = classnames( className, {
-		[ `is-content-justification-${ justifyContent }` ]: justifyContent,
-		[ `is-${ orientation }` ]: orientation,
-	} );
+	const wrapperClasses = classnames(
+		className,
+		{
+			[ `is-content-justification-${ justifyContent }` ]: justifyContent,
+			[ `is-${ orientation }` ]: orientation,
+		},
+		POSITION_CLASSNAMES[ contentPosition ]
+	);
 
 	const blockProps = useBlockProps( { className: wrapperClasses } );
 
@@ -55,6 +75,17 @@ function ButtonsEdit( props ) {
 
 	return (
 		<>
+			<BlockControls group="block">
+				<BlockAlignmentMatrixControl
+					label={ __( 'Change content position' ) }
+					value={ contentPosition }
+					onChange={ ( nextPosition ) =>
+						setAttributes( {
+							contentPosition: nextPosition,
+						} )
+					}
+				/>
+			</BlockControls>
 			<div { ...innerBlocksProps } />
 		</>
 	);
--- src/blocks/buttons/save.js
+++ src/blocks/buttons/save.js
@@ -8,17 +8,33 @@ import classnames from 'classnames';
  */
 import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
 
+const POSITION_CLASSNAMES = {
+	'top left': 'is-position-top-left',
+	'top center': 'is-position-top-center',
+	'top right': 'is-position-top-right',
+	'center left': 'is-position-center-left',
+	'center center': 'is-position-center-center',
+	'center right': 'is-position-center-right',
+	'bottom left': 'is-position-bottom-left',
+	'bottom center': 'is-position-bottom-center',
+	'bottom right': 'is-position-bottom-right',
+};
+
 export default function save( props ) {
 	const {
-		attributes: { layout = {} },
+		attributes: { layout = {}, contentPosition },
 		className,
 	} = props;
 	const { orientation, justifyContent } = layout;
 
-	const wrapperClasses = classnames( className, {
-		[ `is-content-justification-${ justifyContent }` ]: justifyContent,
-		[ `is-${ orientation }` ]: orientation,
-	} );
+	const wrapperClasses = classnames(
+		className,
+		{
+			[ `is-content-justification-${ justifyContent }` ]: justifyContent,
+			[ `is-${ orientation }` ]: orientation,
+		},
+		POSITION_CLASSNAMES[ contentPosition ]
+	);
 
 	const blockProps = useBlockProps.save( { className: wrapperClasses } );
 	const innerBlocksProps = useInnerBlocksProps.save( blockProps );
